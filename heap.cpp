d69617446bef962d3f45c2598e6f1ef5fa2eea51
jnxyatmjx <jnxyatmjx@qq.com>
2021-05-08 18:55:36 +0800

    fix heap print

--- heap.cpp
+++ heap.cpp
@@ -33,16 +33,29 @@ SIZE heapHeight(INDEX root,SIZE maxnum)
 }
 
 template <typename T,typename INDX>
-void heapPrintf(T* li,INDX count,char ** res)
+void heapPrintUpdate(INDX root,T* li,char ** res,INDX count,INDX row,INDX left,INDX right)
+{
+	if(root >= count) return;
+	INDX mid = left + (right - left)/2;
+	res[row][mid] = li[root];
+	heapPrintUpdate(2*root+1,li,res,count,row+1,left,mid - 1);
+	heapPrintUpdate(2*root+2,li,res,count,row+1,mid + 1,right);
+}
+	
+	
+template <typename T,typename INDX>
+void heapPrint(T* li,INDX count,char ** res)
 {
 	INDX heigh = heapHeight<INDX,INDX>(0,count);
 	INDX width = (INDX)(::pow(2,heigh) - 1);
+	heapPrintUpdate(0,li,res,count,0,0,width-1);
 }
 
 
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <vector>
 int main(int argc,char* argv[])
 {	
 	if (argc < 2) {printf("argc = 2 \n");return 0;}
@@ -56,12 +69,34 @@ int main(int argc,char* argv[])
 	//build heap
 	for(int i = n/2 -1 ;i >= 0 ;i--)
 		heapifyUpToDown<int,int>(res,n,i);
-
-	printf("heap height:%d pow(2):%f\n",heapHeight<int,int>(0,n),::pow(2,3));
 	
+	//malloc res )))
+	int row = heapHeight<int,int>(0,n);
+	int colum = (int)::pow(2,row) - 1;
+	char **  ans = (char **)malloc(sizeof(char*) * row);
+	for(int i = 0 ;i < row;i++) ans[i] = (char*)malloc(sizeof(char) * colum);
+	
+	//set value
+	for(int i = 0;i < row;i++)
+		for(int j = 0 ; j < colum;j++)
+			ans[i][j] = ' ';
+
 	for(int i = 0 ;i < n ;i++)
 		printf("%d ",res[i]);	
-		printf("\n");	
+		printf("\n");
+
+
+	heapPrint<int,int>(res,n,ans);
+
+	for(int i = 0;i < row;i++)
+	{
+		for(int j = 0 ; j < colum;j++)
+			printf("%c ",ans[i][j]);
+		printf("--\n");
+	}
+
+	printf("\n");
+
 
 	return 0;
 }

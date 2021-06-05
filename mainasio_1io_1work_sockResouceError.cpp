2ae3aaa82b181a6572f5c709e8147d77fc0229a7
jnxyatmjx <jnxyatmjx@qq.com>
2021-06-05 17:09:34 +0800

    add asio shared_ptr notation

--- mainasio_1io_1work_sockResouceError.cpp
+++ mainasio_1io_1work_sockResouceError.cpp
@@ -33,7 +33,7 @@ public:
   
   ~session(void)
   {
-    printf("Session %p has be Delete.. socket:%p\n",this,&socket_);
+    //printf("Session %p has be Delete.. socket:%p\n",this,&socket_);
   }
 
   void start()
@@ -45,7 +45,7 @@ private:
   void do_read()
   {
     auto self(shared_from_this()); 
-    //printf("do_read %p share count:%td tid:%td\n",&socket_,self.use_count(),syscall(SYS_gettid));
+    printf("do_read %p share count:%td tid:%td\n",&socket_,self.use_count(),syscall(SYS_gettid));
     socket_.async_read_some(asio::buffer(data_, max_length),
         [self,this](std::error_code ec, std::size_t length)
         {
@@ -84,26 +84,62 @@ void do_accept(tcp::acceptor& acc,asio::io_service &iosvr)
           if (!ec)
           {
             //std::make_shared<session>(std::move(socket_))->start();
+            //上句 与 以下两句 作用相同
+
+            // 创建类 sessoin智能指针对象
             std::shared_ptr<session> okpt( new session(std::move(socket_)) );
-            printf("Session %p has be Constr.. socket:%p %td\n",okpt.get(),&socket_,okpt.use_count());
-            okpt->start();
+            okpt->start();// 通过shared_from_this() 创建类 session 智能指针对象
           }
 
           do_accept(acc,iosvr);
         });
   }
 
+class A : public std::enable_shared_from_this<A>
+{
+public:
+  A(){printf("Construct A\n");}
+  ~A(){printf("Destruct A \n");}
+
+/*
+  WHY --> do_read  share count:2
+
+*/
+  void startA()
+  {
+    auto self(shared_from_this());
+      printf("class A cout:%td\n",self.use_count()); //class A cout:2
+  }
+};
+
 int main(int argc, char* argv[])
 {
+
+  {
+    // std::shared_ptr<A> a(new A()); //创建 A对象智能指针
+    // a->startA();                   //调用 shared_from_this() 又创建A对象智能指针
+    
+    /*
+        |||
+        |||
+    */ 
+    std::make_shared<A>()->startA();
+
+    //所以 class A cout:2
+  }
+
+  int port = -1;
   try
   {
-    if (argc != 2)
+    if (argc < 2)
     {
-      std::cerr << "Usage: async_tcp_echo_server <port>\n";
-      return 1;
+      std::cerr << "Usage: async_tcp_echo_server <port> 8970\n";
+      port = 8970;
+    }else
+    {
+      port = atoi(argv[1]);
     }
   printf("Main threadId:%td\n",syscall(SYS_gettid));
-  int port = atoi(argv[1]);
 
   asio::io_service accetp_iosvr_;
   asio::io_service work_iosvr_;

dddffdaac2c999c2a085ff254c70008d5e7299a5
jnxyatmjx <jnxyatmjx@qq.com>
2021-06-11 18:03:22 +0800

    add longest-substring-norepeat

---
+++ submit/3.longest-substring-without-repeating-characters.java
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode id=3 lang=java
+ *
+ * [3] Longest Substring Without Repeating Characters
+ */
+
+// @lc code=start
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int n = s.length(), ans = 0;
+        int[] index = new int[128]; // current index of character ,All ASCII character
+        // try to extend the range [i, j]
+        for (int j = 0, i = 0; j < n; j++) {
+            i = Math.max(index[s.charAt(j)], i);
+            ans = Math.max(ans, j - i + 1);
+            index[s.charAt(j)] = j + 1;
+        }
+        return ans;
+    }
+}
+// @lc code=end
+

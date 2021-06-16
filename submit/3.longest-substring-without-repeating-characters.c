3d0efeedea463f84e84c63e892cf85e0871d24fc
jnxyatmjx <jnxyatmjx@qq.com>
2021-06-16 09:44:25 +0800

    modify 3 longest-substring-character-c

--- submit/3.longest-substring-without-repeating-characters.c
+++ submit/3.longest-substring-without-repeating-characters.c
@@ -12,15 +12,21 @@
    a_ > b_ ? a_ : b_;\
 })
 
+#define lmin(a,b)\
+({ __typeof__ (a) a_ = (a);\
+    __typeof__ (b) b_ = (b);\
+    a_ < b_ ? a_ : b_;})
+
 int lengthOfLongestSubstring(char * s){
     int n = strlen(s), ans = 0;
     int lookup[128]; memset(lookup,0,sizeof(int) * 128);// current lookup of character ,All ASCII character
     
-    // try to extend the range [i, j]
-    for (int j = 0, i = 0; j < n; j++) {
-        i = lmax(lookup[s[j]], i);
-        ans = lmax(ans, j - i + 1);
-        lookup[s[j]] = j + 1;
+    // try to extend the range [left, i]
+    for (int i = 0, left = 0; i < n; i++) {
+        left = lmax(lookup[s[i]], left);
+        ans = lmax(ans, i - left + 1);
+        lookup[s[i]] = i + 1;
+        //printf("left:%d i:%d lookup[%d]:%d\n",left,i,s[i],lookup[s[i]]);
     }
     return ans;
 }

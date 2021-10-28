762afbd4485b3264fd2ef4ae8901827e05ad8c58
jnxyatmjx <jnxyatmjx@qq.com>
2021-10-28 18:25:30 +0800

    add permutation for cpp

---
+++ submit/46.permutations.cpp
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode id=46 lang=cpp
+ *
+ * [46] Permutations
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& num) {
+        vector<vector<int>> res;
+        vector<int> out, visited(num.size(), 0);
+        permuteDFS(num, 0, visited, out, res);
+        return res;
+    }
+    void permuteDFS(vector<int>& num, int level, vector<int>& visited, vector<int>& out, vector<vector<int>>& res) {
+        
+        //level is recored current number of visited
+        if (level == num.size()) {
+                res.push_back(out); 
+                return;
+            }
+
+        for (int i = 0; i < num.size(); ++i) {
+            if (visited[i] == 1) 
+                continue;
+
+            visited[i] = 1;
+            out.push_back(num[i]); //make choice
+
+            permuteDFS(num, level + 1, visited, out, res);
+
+            out.pop_back(); //cancel the choice
+            visited[i] = 0;
+        }//end for
+    }
+};
+
+// @lc code=end
+

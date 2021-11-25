7f368e2d59f36c697810c0b9e2347ed3235adac8
jnxyatmjx <jnxyatmjx@qq.com>
2021-11-25 15:40:47 +0800

    add number to hexadecimal

---
+++ submit/405.convert-a-number-to-hexadecimal.c
@@ -0,0 +1,39 @@
+/*
+ * @lc app=leetcode id=405 lang=c
+ *
+ * [405] Convert a Number to Hexadecimal
+ */
+
+// @lc code=start
+
+void reverse(char*res,int l,int r)
+{
+    if (l >= r) return ;
+    char temp = res[l];
+    res[l] = res[r];
+    res[r] = temp;
+    reverse(res,l+1,r-1);
+}
+
+char * toHex(int num){
+    
+    char* res = (char*)calloc(sizeof(int)*8,sizeof(char));
+    if(num == 0) 
+    {
+        res[0] = '0';
+        return res;
+    }
+    int idx = 0,numsd=0;
+
+    char symbol[] = {'0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'};
+    
+    while(num != 0 && numsd++ < (sizeof(int)*8)/4 ) /*num>=0 is ERROR ,when negative*/
+    {   
+        res[idx++] = symbol[num&15]; //num%16 is error, because -1 % 16 = -1
+        num = num >> 4;
+    }
+    reverse(res,0, strlen(res)-1);
+    return res;
+}
+// @lc code=end
+

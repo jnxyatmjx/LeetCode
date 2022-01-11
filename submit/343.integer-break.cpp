4cdc87b201cf7b20cc23ae7a7b8034c4dcf08d81
jnxyatmjx <jnxyatmjx@qq.com>
2022-01-11 20:00:59 +0800

    add comment for break integer

--- submit/343.integer-break.cpp
+++ submit/343.integer-break.cpp
@@ -12,7 +12,14 @@ public:
         dp[2] = 1;
         for (int i = 3; i <= n ; i++) {
             for (int j = 1; j < i - 1; j++) {
-                dp[i] = max(dp[i], max((i - j) * j, dp[i - j] * j));
+                /*
+                    2 solutions
+                    first : Product of two numbers. {e.g. j and (i-j) }
+                    second: Product of multiple numbers. {e.g. j and dp[i-j] }
+                    dp[i-j] means maximum product value of number (i-j). 
+                    So j multiplied dp[i-j] is all possible results
+                */
+                dp[i] = max(dp[i], max( (i-j)*j, dp[i-j]*j) );
             }
         }
         return dp[n];
